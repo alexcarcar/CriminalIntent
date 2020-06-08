9911885837ad8dac6c7c1545c0cb73cfcf71e62d
acarcar <alexcarcar@yahoo.com>
2020-06-07 20:02:49 -0700

    Chapter 11: Databases and the Room Library

---
+++ app/src/main/java/alex/carcar/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,17 @@
+package alex.carcar.criminalintent.database
+
+import alex.carcar.criminalintent.Crime
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import java.util.*
+
+@Dao
+interface CrimeDao {
+
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): LiveData<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): LiveData<Crime?>
+}
---
+++ app/src/main/java/alex/carcar/criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,12 @@
+package alex.carcar.criminalintent.database
+
+import alex.carcar.criminalintent.Crime
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+    abstract fun crimeDao(): CrimeDao
+}
